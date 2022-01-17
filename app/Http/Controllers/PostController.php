dfa3cacb439baca002314398e83e3e4733910e09
jaime <jaimereycasado@gmail.com>
2022-01-17 15:02:17 +0100

    Caché 01

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,12 +6,25 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\Tag;
+use Illuminate\Support\Facades\Cache;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::where('status', 2)->latest('id')->paginate(8);
+        if (request()->page) {
+            $key = 'posts' . request()->page;
+        } else{
+            $key = 'posts';
+        }
+        if (Cache::has($key)) {
+            $posts = Cache::get($key);
+        } else {
+
+            $posts = Post::where('status', 2)->latest('id')->paginate(8);
+            Cache::put($key, $posts);
+        }
+
         return view('posts.index', compact('posts'));
     }
     public function show(Post $post)
@@ -19,26 +32,28 @@ class PostController extends Controller
         $this->authorize('published', $post);
         $similares = Post::where('category_id', $post->category_id)
             ->where('status', 2)
-            ->where('id','!=', $post->id)
+            ->where('id', '!=', $post->id)
             ->latest('id')
             ->take(4)
             ->get();
         return view('posts.show', compact('post', 'similares'));
     }
 
-    public function category(Category $category){
-        $posts = Post::where('category_id',$category->id)
-        ->where('status', 2)
-        ->latest('id')
-        ->paginate(6);
+    public function category(Category $category)
+    {
+        $posts = Post::where('category_id', $category->id)
+            ->where('status', 2)
+            ->latest('id')
+            ->paginate(6);
 
         return view('posts.category', compact('posts', 'category'));
     }
-    public function tag(Tag $tag){
+    public function tag(Tag $tag)
+    {
         $posts = $tag->posts()
-        ->where('status', 2)
-        ->latest('id')
-        ->paginate(4);
+            ->where('status', 2)
+            ->latest('id')
+            ->paginate(4);
 
         return view('posts.tag', compact('posts', 'tag'));
     }
